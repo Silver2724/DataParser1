184f42215c169f97a331e23d30e29d6583242c50
Owner <diya2724@gmail.com>
2019-03-07 08:32:37 -0800

    add getters and setters to ElectionResult class

---
+++ src/ElectionResult.java
@@ -0,0 +1,89 @@
+public class ElectionResult {
+
+    double votes_dem;
+    double votes_gop;
+    double total_votes;
+    double per_dem;
+    double per_gop;
+    String diff; // with quotes
+    double per_point_diff; // percent
+    String state_abbr; // without quotes
+    String county_name; // without quotes
+    int combined_fips;
+
+    public double getTotal_votes() {
+        return total_votes;
+    }
+
+    public void setTotal_votes(double total_votes) {
+        this.total_votes = total_votes;
+    }
+
+    public double getPer_dem() {
+        return per_dem;
+    }
+
+    public void setPer_dem(double per_dem) {
+        this.per_dem = per_dem;
+    }
+
+    public double getPer_gop() {
+        return per_gop;
+    }
+
+    public void setPer_gop(double per_gop) {
+        this.per_gop = per_gop;
+    }
+
+    public String getDiff() {
+        return diff;
+    }
+
+    public void setDiff(String diff) {
+        this.diff = diff;
+    }
+
+    public double getPer_point_diff() {
+        return per_point_diff;
+    }
+
+    public void setPer_point_diff(double per_point_diff) {
+        this.per_point_diff = per_point_diff;
+    }
+
+    public String getState_abbr() {
+        return state_abbr;
+    }
+
+    public void setState_abbr(String state_abbr) {
+        this.state_abbr = state_abbr;
+    }
+
+    public String getCounty_name() {
+        return county_name;
+    }
+
+    public void setCounty_name(String county_name) {
+        this.county_name = county_name;
+    }
+
+    public int getCombined_fips() {
+        return combined_fips;
+    }
+
+    public void setCombined_fips(int combined_fips) {
+        this.combined_fips = combined_fips;
+    }
+
+    public double getVotes_dem() {
+        return votes_dem;
+    }
+
+    public void setVotes_dem(double votes_dem) {
+        this.votes_dem = votes_dem;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
