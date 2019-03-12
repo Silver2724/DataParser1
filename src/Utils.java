e02a930d9e864e9017e7b4496071a73c36203ad3
dbhandarkar748 <diya2724@gmail.com>
2019-03-12 10:53:25 -0700

    fix parse2016ElectionResults

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -18,5 +19,28 @@ public class Utils {
         }
 
         return output.toString();
+    }
+
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResult> electionResults = new ArrayList<>();
+        data.split("\n");
+        for (int i = 1; i < data.length(); i++) {
+            data.split(",");
+            if(data.contains("")) {
+                removeSymbols(data, electionResults);
+            }
+            if(data.contains("%")){
+                removeSymbols(data, electionResults);
+            }
+            ElectionResult result = new ElectionResult();
+            electionResults.add(result);
+        }
+        return electionResults;
+    }
+
+    private static void removeSymbols(String data, ArrayList<ElectionResult> electionResults) {
+        //removes quotes, perecnt signs, etc.
+        electionResults.remove("%");
+        electionResults.remove("");
     }
 }
