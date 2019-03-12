f97a0a3230bf7086e5d74b52f11610f2c3a4181e
dbhandarkar748 <diya2724@gmail.com>
2019-03-12 10:26:54 -0700

    add getters and setter to these six classes

---
+++ src/DataManager.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class DataManager {
+
+    private List<State> states;
+
+    public List<State> getStates() {
+        return states;
+    }
+
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
+}
