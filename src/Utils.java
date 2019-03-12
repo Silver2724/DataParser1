605ce93ab53b988dd86cebfbb56e19616b23aa8a
dbhandarkar748 <diya2724@gmail.com>
2019-03-12 11:11:09 -0700

    clean up data and replace quotes, percent signs, etc.

--- src/Utils.java
+++ src/Utils.java
@@ -26,21 +26,82 @@ public class Utils {
         data.split("\n");
         for (int i = 1; i < data.length(); i++) {
             data.split(",");
-            if(data.contains("")) {
-                removeSymbols(data, electionResults);
-            }
-            if(data.contains("%")){
-                removeSymbols(data, electionResults);
-            }
+            cleanLine(data);
             ElectionResult result = new ElectionResult();
             electionResults.add(result);
         }
         return electionResults;
     }
 
-    private static void removeSymbols(String data, ArrayList<ElectionResult> electionResults) {
-        //removes quotes, perecnt signs, etc.
-        electionResults.remove("%");
-        electionResults.remove("");
+    private static String cleanLine(String row) {
+        int firstQuote = row.indexOf("\"");
+        int secondQuote = row.indexOf("\"", firstQuote + 1);
+
+        while(firstQuote != -1 && secondQuote != -1) {
+            row = cleanSubstring(row, firstQuote, secondQuote);
+
+            firstQuote = row.indexOf("\"");
+            secondQuote = row.indexOf("\"", firstQuote + 1);
+        }
+        return row;
     }
+
+    private static String cleanSubstring(String row, int firstQuote, int secondQuote) {
+        String before = row.substring(0, firstQuote);
+        String after = row.substring(secondQuote + 1);
+
+        String toClean = row.substring(firstQuote + 1, secondQuote);
+
+        return before + clean(toClean) + after;
+    }
+
+    private static String clean(String toClean) {
+        toClean = toClean.replaceAll(",", "").trim();
+        toClean = toClean.replaceAll("%", "");
+
+        return toClean;
+    }
+
+    /*boolean isInteger(String str) {
+
+int n = 0;
+
+try {
+
+n = Integer.parseInt(str);
+
+} catch (NumberFormatException e) {
+
+return false;
+
+}
+
+return true;
+
+}
+
+boolean containsLetters(String str) { … }
+
+boolean containsAnyOf(String str, int start, int end, String chars)
+
+String[] vals = line.split(“,”);
+
+if (isValidToParse( vals )) {
+
+// do it & add object
+
+}
+
+…
+
+boolean isValidToParse( String[] vals ) {
+
+if ( ! isInteger( vals[3] ) ) return false;
+
+if ( vals[4].length() < 1) return false;
+
+if ( containsAnyOf( vals[5], 3, 6, “,\”%”)) return false;
+
+return true;
+}*/
 }
